df7947fb0737c5b00a1e413b4d6047b36950b7f9
Vdg <nicolas.hbt@pm.me>
2022-02-04 16:42:49 +0100

    add artist and song entities

---
+++ MusiumDAL/Entities/ArtistEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusiumDAL.Entities
+{
+    public class ArtistEntity
+    {
+        public ArtistEntity(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MusiumDAL/Entities/SongEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusiumDAL.Entities
+{
+    public class SongEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public string? FilePath { get; set; }
+        public int? Duration { get; set; }
+        public int? GenreId { get; set; }
+    }
+}
