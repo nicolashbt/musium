350c62d4793a6c5999441dfa2443dbc788094f92
Vdg <nicolas.hbt@pm.me>
2022-02-04 11:23:34 +0100

    add primary key

--- MusiumDB/Tables/ArtistSong.sql
+++ MusiumDB/Tables/ArtistSong.sql
@@ -2,6 +2,7 @@
 (
 	[artistId] INT NOT NULL, 
     [songId] INT NOT NULL, 
+    CONSTRAINT [PK_ArtistSong] PRIMARY KEY ([songId],[artistId]),
     CONSTRAINT [FK_ArtistSong_Artist] FOREIGN KEY ([artistId]) REFERENCES [Artist]([id]) ,
     CONSTRAINT [FK_ArtistSong_Song] FOREIGN KEY ([songId]) REFERENCES [Song]([id])
 )
