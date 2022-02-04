7aa6fd800d15f93b60db14a3b49da42406626484
Vdg <nicolas.hbt@pm.me>
2022-02-04 12:01:50 +0100

    update DBFiller to add sample song with multiple artists

---
+++ MusiumDB/DBFiller.sql
@@ -0,0 +1,16 @@
+﻿EXEC AddArtist 'BADBADNOTGOOD'
+EXEC AddSong "Can't Leave the Night"
+EXEC AddArtistSong 1, 1
+
+EXEC AddArtist 'GoGo Penguin'
+EXEC AddSong "Transient State"
+EXEC AddSong "Reactor", 1
+EXEC AddArtistSong 2, 2
+EXEC AddArtistSong 3, 2
+
+
+EXEC AddArtist 'Bicep'
+EXEC AddArtist 'Clara La San'
+EXEC AddSong "Saku"
+EXEC AddArtistSong 4, 3
+EXEC AddArtistSong 4, 4
