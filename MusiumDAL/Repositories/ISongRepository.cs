ac426c5a08d97cd58ffd203bdd3299b7a4e55ba7
Vdg <nicolas.hbt@pm.me>
2022-02-04 16:43:30 +0100

    add Song repository

---
+++ MusiumDAL/Repositories/ISongRepository.cs
@@ -0,0 +1,18 @@
+﻿using MusiumDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusiumDAL.Repositories
+{
+    public interface ISongRepository
+    {
+        public IEnumerable<SongEntity> GetAllSongs();
+        public SongEntity GetSong(int id);
+        public void DeleteSong(int id);
+        public void AddSong(SongEntity songEntity, IEnumerable<ArtistEntity> artistEntities);
+        public void UpdateSong(SongEntity songEntity);
+    }
+}
