122c6b3aec0f33946b660aebdd3114892f9ec174
Vdg <nicolas.hbt@pm.me>
2022-02-07 11:07:43 +0100

    add mappers for Artist and Song

---
+++ MusiumAPI/Mappers/ArtistMapper.cs
@@ -0,0 +1,24 @@
+﻿using DAL = MusiumDAL.Entities;
+using API = MusiumAPI.Models;
+namespace MusiumAPI.Mappers
+{
+    public static class ArtistMapper
+    {
+        public static API.Artist MapToApi(this DAL.ArtistEntity artist)
+        {
+            return new API.Artist
+            {
+                Id = artist.Id,
+                Name = artist.Name
+            };
+        }
+        public static DAL.ArtistEntity MapToDal(this API.ArtistForm artistForm)
+        {
+            return new DAL.ArtistEntity
+            {
+                Id = artistForm.Id,
+                Name = artistForm.Name
+            };
+        }
+    }
+}
---
+++ MusiumAPI/Mappers/SongMapper.cs
@@ -0,0 +1,32 @@
+﻿using DAL = MusiumDAL.Entities;
+using API = MusiumAPI.Models;
+namespace MusiumAPI.Mappers
+{
+    public static class SongMapper
+    {
+        public static API.Song MapToApi(this DAL.SongEntity song)
+        {
+            return new API.Song
+            {
+                Id = song.Id,
+                Name = song.Name,
+                IsActive = song.IsActive,
+                FilePath = song.FilePath,
+                Duration = song.Duration,
+                GenreId = song.GenreId
+            };
+        }
+        public static DAL.SongEntity MapToDal(this API.SongForm songForm)
+        {
+            return new DAL.SongEntity
+            {
+                Id = songForm.Id,
+                Name = songForm.Name,
+                IsActive = songForm.IsActive,
+                FilePath = songForm.FilePath,
+                Duration = songForm.Duration,
+                GenreId = songForm.GenreId
+            };
+        }
+    }
+}
