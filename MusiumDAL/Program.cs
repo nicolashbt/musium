1057e824e49ca8ec904f051f9a1723a3c398fa7c
Vdg <nicolas.hbt@pm.me>
2022-02-07 16:08:18 +0100

    comment the testing lines

--- MusiumDAL/Program.cs
+++ MusiumDAL/Program.cs
@@ -12,49 +12,49 @@ namespace MusiumDAL
     {
         static void Main(string[] args)
         {
-            SongService songService = new SongService();
-            ArtistService artistService = new ArtistService();
+            //SongService songService = new SongService();
+            //ArtistService artistService = new ArtistService();
 
-            //Testing GetAllSongs
-            IEnumerable<SongEntity> songs = songService.GetAllSongs();
-            foreach (SongEntity songEntity in songs)
-            {
-                Console.WriteLine("Song name: " + songEntity.Name + " - id: " + songEntity.Id);
-            }
+            ////Testing GetAllSongs
+            //IEnumerable<SongEntity> songs = songService.GetAllSongs();
+            //foreach (SongEntity songEntity in songs)
+            //{
+            //    Console.WriteLine("Song name: " + songEntity.Name + " - id: " + songEntity.Id);
+            //}
 
-            //Testing AddArtist
-            List<int> artistsIds = new List<int>();
-            int id1 = artistService.AddArtist(new ArtistEntity()
-            {
-                Name = "artist1"
-            });
-            int id2 = artistService.AddArtist(new ArtistEntity()
-            {
-                Name = "artist2"
-            });
-            artistsIds.Add(id1);
-            artistsIds.Add(id2);
-            //Testing GetAllArtists
-            IEnumerable<ArtistEntity> artists = artistService.GetAllArtists();
-            foreach (ArtistEntity artistEntity in artists)
-            {
-                Console.WriteLine("Artist name: " + artistEntity.Name + " - id: " + artistEntity.Id);
-            }
+            ////Testing AddArtist
+            //List<int> artistsIds = new List<int>();
+            //int id1 = artistService.AddArtist(new ArtistEntity()
+            //{
+            //    Name = "artist1"
+            //});
+            //int id2 = artistService.AddArtist(new ArtistEntity()
+            //{
+            //    Name = "artist2"
+            //});
+            //artistsIds.Add(id1);
+            //artistsIds.Add(id2);
+            ////Testing GetAllArtists
+            //IEnumerable<ArtistEntity> artists = artistService.GetAllArtists();
+            //foreach (ArtistEntity artistEntity in artists)
+            //{
+            //    Console.WriteLine("Artist name: " + artistEntity.Name + " - id: " + artistEntity.Id);
+            //}
 
-            //Testing AddSongs
-            SongEntity songEntity1 = new SongEntity()
-            {
-                Name = "song1"
-            };
-            songService.AddSong(songEntity1, artistsIds);
-            Console.WriteLine("added a song");
+            ////Testing AddSongs
+            //SongEntity songEntity1 = new SongEntity()
+            //{
+            //    Name = "song1"
+            //};
+            //songService.AddSong(songEntity1, artistsIds);
+            //Console.WriteLine("added a song");
 
-            //Testing GetAllSongs
-            songs = songService.GetAllSongs();
-            foreach (SongEntity songEntity in songs)
-            {
-                Console.WriteLine("Song name: " + songEntity.Name + " - id: " + songEntity.Id);
-            }
+            ////Testing GetAllSongs
+            //songs = songService.GetAllSongs();
+            //foreach (SongEntity songEntity in songs)
+            //{
+            //    Console.WriteLine("Song name: " + songEntity.Name + " - id: " + songEntity.Id);
+            //}
         }
     }
 }
