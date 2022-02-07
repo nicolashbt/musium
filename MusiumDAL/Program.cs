90dfb5cd204e48136378c0d2f66ff302d9c9c873
Vdg <nicolas.hbt@pm.me>
2022-02-07 10:21:39 +0100

    more testing

--- MusiumDAL/Program.cs
+++ MusiumDAL/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MusiumDAL.Entities;
+using MusiumDAL.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,46 @@ namespace MusiumDAL
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            SongService songService = new SongService();
+            ArtistService artistService = new ArtistService();
+
+            //Testing GetAllSongs
+            IEnumerable<SongEntity> songs = songService.GetAllSongs();
+            foreach (SongEntity songEntity in songs)
+            {
+                Console.WriteLine("Song name: " + songEntity.Name + " - id: " + songEntity.Id);
+            }
+
+            //Testing AddArtist
+            artistService.AddArtist(new ArtistEntity()
+            {
+                Name = "artist1"
+            });
+            artistService.AddArtist(new ArtistEntity()
+            {
+                Name = "artist2"
+            });
+            //Testing GetAllArtists
+            IEnumerable<ArtistEntity> artists = artistService.GetArtists();
+            foreach (ArtistEntity artistEntity in artists)
+            {
+                Console.WriteLine("Artist name: " + artistEntity.Name + " - id: " + artistEntity.Id);
+            }
+
+            //Testing AddSongs
+            SongEntity songEntity1 = new SongEntity()
+            {
+                Name = "song1"
+            };
+            songService.AddSongWithArtist(songEntity1, artists);
+            Console.WriteLine("added a song");
+
+            //Testing GetAllSongs
+            songs = songService.GetAllSongs();
+            foreach (SongEntity songEntity in songs)
+            {
+                Console.WriteLine("Song name: " + songEntity.Name + " - id: " + songEntity.Id);
+            }
         }
     }
 }
