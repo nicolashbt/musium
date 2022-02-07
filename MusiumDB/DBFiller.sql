e504578b3240bb136f8ee5c956ff70a6798ee8db
Vdg <nicolas.hbt@pm.me>
2022-02-07 10:20:49 +0100

    reflect change of add ouput id from AddArtist

--- MusiumDB/DBFiller.sql
+++ MusiumDB/DBFiller.sql
@@ -1,16 +1,16 @@
-﻿EXEC AddArtist 'BADBADNOTGOOD'
+﻿EXEC AddArtist 'BADBADNOTGOOD',0
 EXEC AddSong "Can't Leave the Night"
 EXEC AddArtistSong 1, 1
 
-EXEC AddArtist 'GoGo Penguin'
+EXEC AddArtist 'GoGo Penguin',0
 EXEC AddSong "Transient State"
 EXEC AddSong "Reactor", 1
 EXEC AddArtistSong 2, 2
 EXEC AddArtistSong 2, 3
 
 
-EXEC AddArtist 'Bicep'
-EXEC AddArtist 'Clara La San'
+EXEC AddArtist 'Bicep',0
+EXEC AddArtist 'Clara La San',0
 EXEC AddSong "Saku"
 EXEC AddArtistSong 3, 4
 EXEC AddArtistSong 4, 4
