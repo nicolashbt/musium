b1584f34d047cc608d5c5e13602536a14d598ff9
Vdg <nicolas.hbt@pm.me>
2022-02-07 10:20:21 +0100

    add output id

--- MusiumDB/StoredProcedures/Artist/AddArtist.sql
+++ MusiumDB/StoredProcedures/Artist/AddArtist.sql
@@ -1,7 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[AddArtist]
-	@name NVARCHAR(50)
+	@name NVARCHAR(50),
+	@id int output
 AS
 BEGIN
 	INSERT INTO Artist(name)
 	VALUES (@name)
+	SELECT @id = @@IDENTITY FROM Artist
 END
