d090449fe9713f786a002aed51a652d3d864f93f
Vdg <nicolas.hbt@pm.me>
2022-02-07 11:29:11 +0100

    add Artist and Song controllers with GET and POST

---
+++ MusiumAPI/Controllers/ArtistController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusiumAPI.Mappers;
+using MusiumAPI.Models;
+using MusiumDAL.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusiumAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistController : ControllerBase
+    {
+        private IArtistRepository _artistRepository;
+
+        public ArtistController(IArtistRepository artistRepository)
+        {
+            _artistRepository = artistRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_artistRepository.GetAllArtists().Select(a => a.MapToApi()));
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetById(int Id)
+        {
+            return Ok(_artistRepository.GetArtist(Id).MapToApi());
+        }
+
+        [HttpPost]
+        public IActionResult Create(ArtistForm artistForm)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            _artistRepository.AddArtist(artistForm.MapToDal());
+            return Ok();
+        }
+    }
+}
---
+++ MusiumAPI/Controllers/SongController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusiumAPI.Mappers;
+using MusiumAPI.Models;
+using MusiumDAL.Repositories;
+
+namespace MusiumAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        private ISongRepository _songRepository;
+
+        public SongController(ISongRepository songRepository)
+        {
+            _songRepository = songRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_songRepository.GetAllSongs().Select(s => s.MapToApi()));
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetById(int Id)
+        {
+            return Ok(_songRepository.GetSong(Id).MapToApi());
+        }
+
+        [HttpPost]
+        public IActionResult Create(SongForm songForm)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            _songRepository.AddSong(songForm.MapToDal(), songForm.ArtistIds);
+            return Ok();
+        }
+    }
+}
