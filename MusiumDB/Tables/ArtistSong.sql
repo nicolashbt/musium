b6de13280f41939a76a7d466112cce2391395a98
Vdg <nicolas.hbt@pm.me>
2022-02-08 16:06:44 +0100

    change column order

--- MusiumDB/Tables/ArtistSong.sql
+++ MusiumDB/Tables/ArtistSong.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[ArtistSong]
 (
-	[artistId] INT NOT NULL, 
     [songId] INT NOT NULL, 
+	[artistId] INT NOT NULL, 
     CONSTRAINT [PK_ArtistSong] PRIMARY KEY ([songId],[artistId]),
     CONSTRAINT [FK_ArtistSong_Artist] FOREIGN KEY ([artistId]) REFERENCES [Artist]([id]) ,
     CONSTRAINT [FK_ArtistSong_Song] FOREIGN KEY ([songId]) REFERENCES [Song]([id])
