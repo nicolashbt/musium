58620f5ba85ebc6360e02b8864263a9e6023b4cf
Vdg <nicolas.hbt@pm.me>
2022-02-08 16:07:09 +0100

    add UpdateArtistSong stored procedure

---
+++ MusiumDB/StoredProcedures/Song/UpdateArtistSong.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[UpdateArtistSong]
+	@songId INT,
+	@artistId T_ArtistId READONLY
+AS
+BEGIN
+	DELETE FROM ArtistSong WHERE songId = @songId
+	INSERT INTO ArtistSong(songId,artistId)
+	SELECT @songId, id FROM @artistId
+END
