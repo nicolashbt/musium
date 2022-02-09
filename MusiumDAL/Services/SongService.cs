d6822da0a89aaf0ac5277bce89b80e81fc45dafa
Vdg <nicolas.hbt@pm.me>
2022-02-09 09:27:23 +0100

    add try catch and  UpdateArtistSong

--- MusiumDAL/Services/SongService.cs
+++ MusiumDAL/Services/SongService.cs
@@ -43,7 +43,6 @@ namespace MusiumDAL.Services
                     {
                         c.Open();
                         int Id = (int)cmd.ExecuteNonQuery();
-                        Console.WriteLine(Id);
                     }
                     catch (SqlException e)
                     {
@@ -99,19 +98,27 @@ namespace MusiumDAL.Services
                 {
                     cmd.CommandText = "SELECT * FROM Song WHERE id=@id";
                     cmd.Parameters.AddWithValue("id", id);
-                    c.Open();
-                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    try
                     {
-                        while (reader.Read())
+                        c.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
                         {
-                            songEntity.Id = (int)reader["id"];
-                            songEntity.Name = (string)reader["name"];
-                            songEntity.IsActive = (bool)reader["isActive"];
-                            songEntity.FilePath = (reader["filePath"] == DBNull.Value) ? null : (string)reader["filePath"];
-                            songEntity.Duration = (reader["duration"] == DBNull.Value) ? null : (int)(reader["duration"]);
-                            songEntity.GenreId = (reader["genreId"] == DBNull.Value) ? null : (int)(reader["genreId"]);
+                            while (reader.Read())
+                            {
+                                songEntity.Id = (int)reader["id"];
+                                songEntity.Name = (string)reader["name"];
+                                songEntity.IsActive = (bool)reader["isActive"];
+                                songEntity.FilePath = (reader["filePath"] == DBNull.Value) ? null : (string)reader["filePath"];
+                                songEntity.Duration = (reader["duration"] == DBNull.Value) ? null : (int)(reader["duration"]);
+                                songEntity.GenreId = (reader["genreId"] == DBNull.Value) ? null : (int)(reader["genreId"]);
+                            }
                         }
                     }
+                    catch (SqlException e)
+                    {
+                        throw new Exception(e.Message);
+                    }
+
                 }
             }
             return songEntity;
@@ -132,11 +139,53 @@ namespace MusiumDAL.Services
                     cmd.Parameters.AddWithValue("duration", songEntity.Duration);
                     cmd.Parameters.AddWithValue("genreId", songEntity.GenreId);
 
-                    c.Open();
-                    int Id = (int)cmd.ExecuteNonQuery();
+                    try
+                    {
+                        c.Open();
+                        int Id = (int)cmd.ExecuteNonQuery();
+                    }
+                    catch (SqlException e)
+                    {
+                        throw new Exception(e.Message);
+                    }
                 }
             }
         }
+
+        public void UpdateArtistSong(int songId, IEnumerable<int> artistIds)
+        {
+            using (SqlConnection c = new SqlConnection())
+            {
+                c.ConnectionString = _winAuthConnectionString;
+                using (SqlCommand cmd = c.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "UpdateArtistSong";
+                    cmd.Parameters.AddWithValue("songId", songId);
+
+
+                    //@artistId T_ArtistId READONLY
+                    DataTable ArtistIds = new DataTable();
+                    ArtistIds.Columns.Add("id", typeof(int));
+                    foreach (int artistId in artistIds)
+                    {
+                        ArtistIds.Rows.Add(artistId);
+                    }
+                    cmd.Parameters.Add(new SqlParameter("artistId", ArtistIds));
+
+                    try
+                    {
+                        c.Open();
+                        int Id = (int)cmd.ExecuteNonQuery();
+                    }
+                    catch (SqlException e)
+                    {
+                        throw new Exception(e.Message);
+                    }
+                }
+            }
+        }
+
         public void DeleteSong(int id)
         {
             using (SqlConnection c = new SqlConnection())
@@ -147,8 +196,16 @@ namespace MusiumDAL.Services
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.CommandText = "DeleteSong";
                     cmd.Parameters.AddWithValue("id", id);
-                    c.Open();
-                    int Id = (int)cmd.ExecuteNonQuery();
+
+                    try
+                    {
+                        c.Open();
+                        int Id = (int)cmd.ExecuteNonQuery();
+                    }
+                    catch (SqlException e)
+                    {
+                        throw new Exception(e.Message);
+                    }
                 }
             }
 
