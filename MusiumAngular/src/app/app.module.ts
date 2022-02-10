f382e8412611138acfb3f89d70215eed13e69b65
Vdg <nicolas.hbt@pm.me>
2022-02-10 16:11:35 +0100

    add index component

--- MusiumAngular/src/app/app.module.ts
+++ MusiumAngular/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ArtistsongEditComponent } from './song/song-edit/artistsong-edit/artist
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { RegisterComponent } from './auth/register/register.component';
     ArtistsongEditComponent,
     AuthComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
