9ec55a6dbe5bf0b90cf0efcba581c826203b1d6c
Nicolas Hbt <nicolas.hbt@pm.me>
2022-02-17 15:48:46 +0100

    default isActive to true

--- MusiumAngular/src/app/song/song.model.ts
+++ MusiumAngular/src/app/song/song.model.ts
@@ -1,11 +1,11 @@
 export class Song {
-    public id!: number;
-    public name!: string;
-    public isActive!: boolean;
-    public filePath !:string;
-    public duration!: number;
-    public genreId!: number;
+  public id!: number;
+  public name!: string;
+  public isActive: boolean = true;
+  public filePath !: string;
+  public duration!: number;
+  public genreId!: number;
 
-constructor(){ }
-  
-  }
+  constructor() { }
+
+}
